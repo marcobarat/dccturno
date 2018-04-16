60b06ca9bd375c639c0157bcb82a4be0950719d5
MarcoPC-TOSH\Marco PC <marco.baratella@techedgegroup.com>
2018-04-16 18:06:58 +0200

    modifica al menu al click del bottone

--- app/controller/ManagePiano.controller.js
+++ app/controller/ManagePiano.controller.js
@@ -1,23 +1,26 @@
 sap.ui.define([
+    'sap/m/MessageToast',
     'jquery.sap.global',
     './Formatter',
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel'
-], function (jQuery, Formatter, Controller, JSONModel) {
+], function (MessageToast, jQuery, Formatter, Controller, JSONModel) {
     "use strict";
 
     var ManagePiano = Controller.extend("myapp.controller.ManagePiano", {
 
+        oModel: null,
+        oModel3: null,
+        prova: null,
         onInit: function () {
 
             var params = jQuery.sap.getUriParameters(window.location.href);
             // set explored app's demo model on this sample
-            var oModel = new JSONModel("./model/linee.json");
-            var oModel2 = new JSONModel("./model/products.json");
-            var oModel3 = new JSONModel("./model/operators.json");
+            this.oModel = new JSONModel("./model/linee.json");
+            this.oModel3 = new JSONModel("./model/operators.json");
 
-            this.getView().setModel(oModel,'linea');
-            this.getView().setModel(oModel,'operatore');
+            this.getView().setModel(this.oModel, 'linea');
+            this.getView().setModel(this.oModel3, 'operatore');
             //this.getView().setModel(oModel2,"prodotto");
 
         },
@@ -26,7 +29,10 @@ sap.ui.define([
 
 
         },
+        pressami: function () {
+            alert("pressssss");
 
+        },
         onToTmpPage: function (event) {
 
             this.getOwnerComponent().getRouter().navTo("tmp");
@@ -34,6 +40,34 @@ sap.ui.define([
         },
         managePiano: function (evt) {
             alert("miao anche a te");
+        },
+        handlePressOpenMenu: function (oEvent) {
+            var oButton = oEvent.getSource();
+
+            // create menu only once
+            if (!this._menu) {
+                this._menu = sap.ui.xmlfragment(
+                        "myapp.view.MenuItemEventing",
+                        this
+                        );
+                this.getView().addDependent(this._menu);
+
+            }
+
+            var eDock = sap.ui.core.Popup.Dock;
+            this.prova = new JSONModel("./model/prova.json");
+            this._menu.setModel(this.prova);
+            this._menu.open(this._bKeyboard, oButton, eDock.BeginTop, eDock.BeginBottom, oButton);
+        },
+
+        handleMenuItemPress: function (oEvent) {
+            var msg = "'" + oEvent.getParameter("item").getText() + "' pressed";
+            MessageToast.show(msg);
+        },
+
+        handleTextFieldItemPress: function (oEvent) {
+            var msg = "'" + oEvent.getParameter("item").getValue() + "' entered";
+            MessageToast.show(msg);
         }
 
     });
