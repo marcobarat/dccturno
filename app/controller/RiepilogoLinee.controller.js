f501c1aaaa31581b0f37ff137cad3f237f321dfd
efasano <edoardo.fasano@techedgegroup.com>
2018-06-11 12:30:10 +0200

    Overview Linee

--- app/controller/RiepilogoLinee.controller.js
+++ app/controller/RiepilogoLinee.controller.js
@@ -8,9 +8,20 @@ sap.ui.define([
     return Controller.extend("myapp.controller.RiepilogoLinee", {
         ModelLinee: new JSONModel(),
         onInit: function () {
-            var that = this; 
-            Library.AjaxCallerData("model/linee_riepilogo.json", function(Jdata){that.ModelLinee.setData(Jdata);});
-            this.getView().setModel(this.ModelLinee, "linee"); 
+            var that = this;
+            Library.AjaxCallerData("model/linee_riepilogo.json", function (Jdata) {
+                that.ModelLinee.setData(Jdata);
+            });
+            this.getView().setModel(this.ModelLinee, "linee");
+        },
+        GoToHome: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("main", true);
+        },
+        GoToSinotticoLinea: function (oEvent) {
+            var oLinea = oEvent.getSource().getText();
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("sinotticoLinea", {lineaPath: oLinea});
         }
 
 
