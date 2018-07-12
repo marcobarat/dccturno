0495bda395d5b8765b04962b87e42179f62053b2
benderlana <battilana.matteo@gmail.com>
2018-07-12 09:43:08 +0200

    last vrs with tiles

--- app/controller/Main.controller.js
+++ app/controller/Main.controller.js
@@ -19,11 +19,11 @@ sap.ui.define([
             this.ISLOCAL = Number(jQuery.sap.getUriParameters().get("ISLOCAL"));
             this.ISLOCALModel.setData({"ISLOCAL": this.ISLOCAL});
             sap.ui.getCore().setModel(this.ISLOCALModel, "ISLOCAL");
-            var that = this;
-            Library.SyncAjaxCallerData("model/JSON_Main.json", function (Jdata) {
-                that.ModelTiles.setData(Jdata);
-            });
-            this.getView().setModel(this.ModelTiles);
+//            var that = this;
+//            Library.SyncAjaxCallerData("model/JSON_Main.json", function (Jdata) {
+//                that.ModelTiles.setData(Jdata);
+//            });
+//            this.getView().setModel(this.ModelTiles);
         },
         onToPianiPage: function () {
             var link;
